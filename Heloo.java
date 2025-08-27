6334276d28850a35821fc5600a88ded943210f63
Prakhar Singh <your_github_email@example.com>
2025-08-27 09:54:49 +0530

    Initial commit

---
+++ Heloo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Heloo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int age = 21;
+        System.out.println("The age is " + age);
+
+        Scanner sc = new Scanner(System.in);
+        int u = sc.nextInt();
+        System.out.println(u);
+    }
+}
